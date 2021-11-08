1ec4351ef5d8ec4073fe74a4729f1eedb194bf2a
haleyej <haleyej@umich.edu>
2021-11-08 12:39:27 -0500

    implemented  muted button

--- js/video.js
+++ js/video.js
@@ -7,13 +7,20 @@ window.addEventListener("load", function() {
 
 });
 
+// implement play button
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
+	console.log("Play Video")
 	video.play()
  });
 
-
+// implement pause button
  document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause Video");
+	console.log("Pause Video")
 	video.pause()
  });
+
+//  mute video
+ document.querySelector("#mute").addEventListener('click', function() {
+	 console.log("Muted Video")
+	 video.muted = true
+ })
