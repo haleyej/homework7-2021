20a7b599422ba0c8f8eb03b2293baa58b3af849f
haleyej <haleyej@umich.edu>
2021-11-08 12:49:24 -0500

    implemented slow  down button

--- js/video.js
+++ js/video.js
@@ -17,10 +17,21 @@ document.querySelector("#play").addEventListener("click", function() {
  document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video")
 	video.pause()
- });
+ })
+
+//  slow down
+
+document.querySelector("#slower").addEventListener("click", function(){
+	console.log("slowing down video")
+	var current = video.playbackRate
+	video.playbackRate = (current * 0.95)
+	console.log(video.playbackRate)
+
+})
 
 //  mute video
  document.querySelector("#mute").addEventListener('click', function() {
 	 console.log("Muted Video")
 	 video.muted = true
  })
+
